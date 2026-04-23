0a5afb21d1c6381902f8b3696c87e928e8597d23
Pham Ngoc Kha <ngockha1900@gmail.com>
2026-04-23 15:26:41 +0700

    Final Fix: Adding package-lock and correct base path

--- .github/vite.config.js
+++ .github/vite.config.js
@@ -1,15 +1,5 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        rollupOptions: {
-            input: {
-                'main': resolve(__dirname, 'index.html'),
-                'coffee-shop': resolve(__dirname, 'apps/coffee-shop/index.html'),
-                'todo-list': resolve(__dirname, 'apps/todo-list/index.html'),
-                'sample-web-components': resolve(__dirname, 'apps/sample-web-components/index.html'),
-            },
-        },
-    },
+  base: '/phamngockha24020002.github.io/',
 })
